7e65db4b607430ef61ac51238d87b98df5629e5a
stefano stamato <sstamato@localhost.localdomain>
2024-12-19 15:50:09 -0800

    example files for docker workshop

---
+++ bob-javascript/calculateInterest.js
@@ -0,0 +1,4 @@
+const totalDebt = 11000000000000000n;
+const annualInterestRate = 5n; // 5%
+const yearlyInterest = (totalDebt * annualInterestRate) / 100n;
+console.log("Yearly Interest:", yearlyInterest);
